39b42388308d7e60d3455a077f3cd534c3af58f4
Jishii <33364545+smudge121@users.noreply.github.com>
2023-01-17 20:08:19 -0800

    Updated user model and added functioning user routes

--- models/User.js
+++ models/User.js
@@ -2,9 +2,38 @@ const { model, Schema } = require("mongoose");
 
 const userSchema = new Schema(
     {
-        username: { type: String, required: true },
+        username: { 
+            type: String, 
+            required: true,
+            unique: true,
+            trim: true
+        },
+        email : {
+            type: String,
+            required: true,
+            unique: true,
+            match: /.+\@.+\..+/
+        },
+        thoughts: [{
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }],
+        friends: [{
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        }
     }
 );
+
+userSchema.virtual('friendCount')
+    .get(function () {
+        return this.friends.length;
+    });
 
 const User = model("User", userSchema);
 
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { User } = require("../../models");
+const { User, Thought } = require("../../models");
 
 router.get("/", async (req, res) => {
     try {
@@ -8,6 +8,43 @@ router.get("/", async (req, res) => {
     } catch (err) {
         res.status(500).json(err);
     }
-})
+});
+
+router.get("/:id", async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+            .populate("thoughts").populate("friends");
+        res.json(user);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.post("/", async (req, res) => {
+    try {
+        const user = await User.create(req.body);
+        res.json(user);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.put("/:id", async (req, res) => {
+    try {
+        await User.findByIdAndUpdate(req.params.id, req.body);
+        res.sendStatus(200);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.delete("/:id", async (req, res) => {
+    try {
+        await User.findByIdAndDelete(req.params.id);
+        res.sendStatus(200);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
