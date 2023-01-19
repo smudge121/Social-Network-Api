7d70a23bc3786ba0d4a2cab28239e36631171c32
Jishii <33364545+smudge121@users.noreply.github.com>
2023-01-18 20:51:04 -0800

    working reaction routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -30,4 +30,45 @@ router.post("/", async(req, res) => {
     }
 });
 
+router.put("/:id", async (req, res) => {
+    try {
+        await Thought.findByIdAndUpdate(req.params.id, req.body);
+        res.sendStatus(200);
+        
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.delete("/:id", async (req, res) => {
+    try {
+        const thought = await Thought.findByIdAndDelete(req.params.id);
+        await User.findOneAndUpdate(thought.username, 
+            { $pull: {thoughts: thought._id}});
+        res.sendStatus(200);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.post("/:thoughtId/reactions", async (req, res) => {
+    try {
+        await Thought.findByIdAndUpdate(req.params.thoughtId, 
+            { $push: {reactions: req.body}});
+        res.sendStatus(200);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.delete("/:thoughtId/reactions/:reactionId", async (req, res) => {
+    try {
+        await Thought.findByIdAndUpdate(req.params.thoughtId, 
+            { $pull: {reactions: {reactionId: req.params.reactionId}}});
+        res.sendStatus(200);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router;
